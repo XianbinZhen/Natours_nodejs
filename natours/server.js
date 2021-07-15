e30a0ee69bc800c0ee5c0b03c0b8e4d7b152358e
XianbinZhen <62436049+XianbinZhen@users.noreply.github.com>
2021-07-15 14:33:20 -0400

    connected with live DB

--- natours/server.js
+++ natours/server.js
@@ -1,5 +1,27 @@
-import app from './app.js'
-const port = 3000;
+import app from './app.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config({ path: './config.env' });
+
+let DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+).replace('<username>', process.env.DATABASE_USER);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true 
+  })
+  .then((connection) => {
+    // console.log(connection);
+    console.log('DB connected 👍');
+  });
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App is running on ' + port);
 });
